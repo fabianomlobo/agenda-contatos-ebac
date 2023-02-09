f936f9ca9b7249c3d868f7b4440363927493ff5e
Fabiano Lobo <fabianofbb9@gmail.com>
2023-02-09 13:29:53 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,46 @@
+const form = document.getElementById('form-contatos')
+const nomes = []
+let linhas = ''
+const nomeContato = document.getElementById('nome-contato')
+const numeroContato = document.getElementById('numero-do-contato')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    if(numeroContato.value.length >= 10 && numeroContato.value.length <= 11){
+        adicionaLinha()
+        atualizaTabela()
+    } else {
+        alert(`A quantidade de caracteres deve ser 10 ou 11!`)
+        limpainput()
+        console.log(numeroContato.value + numeroContato.value)
+    }
+})
+
+function adicionaLinha(){
+    
+        if (nomes.includes(nomeContato.value)){
+            alert(`O contato ${nomeContato.value} ja foi adicionado`)
+        } else {
+            nomes.push(nomeContato.value)    
+        
+            let linha = '<tr>'
+            linha += `<td>${nomeContato.value}</td>`
+            linha += `<td>${numeroContato.value}</td>`
+            linha += `</tr>`
+            linhas += linha
+        } 
+        
+        limpainput()
+}
+
+function limpainput(){
+    nomeContato.value = ''
+    numeroContato.value = ''
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas
+}
+
